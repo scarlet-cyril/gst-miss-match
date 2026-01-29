c06264f724ae2b043ff0a9c7e91c885d83cfaecc
scarlet-cyril <115544459+scarlet-cyril@users.noreply.github.com>
2026-01-30 00:27:21 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="GST ITC Mismatch", layout="centered")
+
+st.title("Snap Invoice → Instant ITC Mismatch")
+
+st.write("This is your first live prototype 🚀")
+
+st.success("If you can see this, your app is working!")
